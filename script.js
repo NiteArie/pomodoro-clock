ea8ae8056a9cc54b4d0f26689b1c9d2652f0c8aa
Nite <solitude.loner7@gmail.com>
2020-12-29 22:35:20 +0700

    [Update] User can change/customize the minutes in both sessions before starting

--- script.js
+++ script.js
@@ -10,7 +10,90 @@ const app = (() => {
     const _optionTimeDisplay = document.querySelector(".container__options__time__content");
     const _optionBreakDisplay = document.querySelector(".container__options__break__content");
 
-    
+    let _break = 5;
+    let _time = 25;
+    let _second = 0;
+
+    _incrementTimeButton.addEventListener("click", event => {
+        incrementTimeByValue(1);
+        validTime();
+        renderTimeDisplay();
+        renderOptionTimeDisplay();
+    })
+
+    _decrementTimeButton.addEventListener("click", event => {
+        decrementTimeByValue(1);
+        validTime();
+        renderTimeDisplay();
+        renderOptionTimeDisplay();
+    })
+
+    _incrementBreakButton.addEventListener("click", event => {
+        incrementBreakByValue(1);
+        validBreak();
+        renderOptionBreakDisplay();
+    })
+
+    _decrementBreakButton.addEventListener("click", event => {
+        decrementBreakByValue(1);
+        validBreak();
+        renderOptionBreakDisplay();
+    })
+
+    renderTimeDisplay();
+    renderOptionBreakDisplay();
+    renderOptionTimeDisplay();
+
+    function renderTimeDisplay() {
+        _timeDisplay.textContent = formatTimeAndSecond(_time, _second);
+    }
+
+    function renderOptionTimeDisplay() {
+        _optionTimeDisplay.textContent = `${_time} mins`;
+    }
+
+    function renderOptionBreakDisplay() {
+        _optionBreakDisplay.textContent = `${_break} mins`;
+    }
+
+    function incrementTimeByValue(value) {
+        _time += value;
+    }
+
+    function decrementTimeByValue(value) {
+        _time -= value;
+    }
+
+    function incrementBreakByValue(value) {
+        _break += value;
+    }
+
+    function decrementBreakByValue(value) {
+        _break -= value;
+    }
+
+    function validTime() {
+        if (_time < 1) {
+            _time = 1;
+        } else if (_time > 60) {
+            _time = 60;
+        }
+    }
+
+    function validBreak() {
+        if (_break < 1) {
+            _break = 1;
+        } else if (_break > 60) {
+            _break = 60;
+        }
+    }
+
+    function formatTimeAndSecond(time, second) {
+        return (
+            `${time < 10 ? "0" + time : time}:${second < 10 ? "0" + second : second}`
+        );
+    }
+
 
 
 })();
