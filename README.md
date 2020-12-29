6551b55c94ebee396097bbf538744c6b863c96d2
Nite <solitude.loner7@gmail.com>
2020-12-29 22:01:44 +0700

    [Update] User Can see a timer for 25 minutes

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ The Pomodoro Technique is a time management method developed by Francesco Cirill
 
 ## User Stories
 
-* [ ] User can see a timer for 25 minutes - the working session
+* [X] User can see a timer for 25 minutes - the working session
 * [ ] After the working session is over, the User can see a timer for 5 minutes - the break session
 * [ ] User can start / pause, stop and reset the timers
 
---
+++ script.js
@@ -0,0 +1,16 @@
+const app = (() => {
+    const _startButton = document.querySelector(".container__clock__buttons__start");   
+    const _stopButton = document.querySelector(".container__clock__buttons__start");
+    const _resetButton = document.querySelector(".container__clock__buttons__reset");
+    const _timeDisplay = document.querySelector(".container__clock__session__time");
+    const _incrementTimeButton = document.querySelector(".container__options__time__inc");
+    const _decrementTimeButton = document.querySelector(".container__options__time__dec");
+    const _incrementBreakButton = document.querySelector(".container__options__break__inc");
+    const _decrementBreakButton = document.querySelector(".container__options__break__dec");
+    const _optionTimeDisplay = document.querySelector(".container__options__time__content");
+    const _optionBreakDisplay = document.querySelector(".container__options__break__content");
+
+    
+
+
+})();
